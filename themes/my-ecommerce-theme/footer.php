737eda056a8b1e61c33d8f0c9bbcec20968e8565
yimm <bunyimkong@gmail.com>
2025-12-15 19:19:53 +0700

    test

---
+++ themes/my-ecommerce-theme/footer.php
@@ -0,0 +1,42 @@
+</main>
+<footer>
+    <div class="footer-content">
+        <div class="footer-infor">
+            <a href=""><img src="<?php echo get_template_directory_uri() . '/images/logo.png'; ?>" /></a>
+            <p class="infor-p">Bringing you the finest Korean products, carefully selected to ensure quality and authenticity. We strive to deliver beauty, care, and happiness with every order</p>
+        </div>
+
+        <div class="footer-infor">
+            <p>Quick Links</p>
+
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Category</a></li>
+                <li><a href="">About us</a></li>
+                <li><a href="">Contacts</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-infor">
+            <p>Blog</p>
+
+            <ul>
+                <li><a href="">Tip & Trick</a></li>
+                <li><a href="">Guides</a></li>
+                <li><a href="">Visit our blog</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-infor">
+            <p>Company</p>
+
+            <ul>
+                <li><a href="">Privacy Policy</a></li>
+                <li><a href="">Term of Service</a></li>
+                <li><a href="">Legal</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body></html>
---
+++ themes/my-ecommerce-theme/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head><?php wp_head(); ?></head>
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+<body <?php body_class(); ?>>
+<header>
+    <nav class="navbar">
+        <div class="nav-logo">
+            <a href="<?php echo home_url(); ?>">
+                <img src="<?php echo get_template_directory_uri() . '/images/logo.png'; ?>" alt="<?php bloginfo('name'); ?>" />
+            </a>
+        </div>
+
+        <div class="nav-links">
+            <ul>
+                <li><a class="active" href="<?php echo home_url(); ?>">Home</a></li>
+                <li><a href="<?php echo home_url('/category'); ?>">Category</a></li>
+                <li><a href="<?php echo home_url('/products'); ?>">Products</a></li>
+                <li><a href="<?php echo home_url('/about-us'); ?>">About us</a></li>
+                <li><a href="<?php echo home_url('/contact'); ?>">Contacts</a></li>
+            </ul>
+        </div>
+
+        <div class="nav-btn">
+            <a href="#"><i class="material-symbols-outlined">shopping_bag</i></a>
+            <a href="#"><i class="material-symbols-outlined">dark_mode</i></a>
+            <a href="#"><i class="material-symbols-outlined">person</i></a>
+        </div>
+    </nav>
+</header>
+<main>
