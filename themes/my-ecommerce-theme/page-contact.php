bfa32e404ad1e9e98e3f3153bd87aba12bb46fbd
bora4-netizen <chorkbora4@gmail.com>
2025-12-15 16:45:55 +0700

    contact-page 40%

---
+++ themes/my-ecommerce-theme/page-contact.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/contact.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+
+<?php get_header(); ?>
+
+<div class="container">
+
+    <h2>
+        Let’s get in touch!
+    </h2>
+    <div class="form">
+        <form class="box-form">
+                <label for="name">Name*</label>
+
+            <input type="text" placeholder="Your Full name">
+                <label for="emmail">Email*</label>
+            <input type="text" placeholder="Your Full name">
+                <label for="Message">Message</label>
+            <textarea name="Message" id="">Message...</textarea>
+        </form>
+    </div>
+    <div class="map">
+
+    </div>
+</div>
