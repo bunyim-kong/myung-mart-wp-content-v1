e79ecdf42b514e8cd84319d70c15013079bfde89
yimm <bunyimkong@gmail.com>
2025-12-18 01:03:05 +0700

    Finish navbar and footer responsive

--- themes/my-ecommerce-theme/header.php
+++ themes/my-ecommerce-theme/header.php
@@ -28,6 +28,41 @@
             <a onclick="toggleTheme(); return false;" href="#"><i class="material-symbols-outlined">dark_mode</i></a>
             <a href="#"><i class="material-symbols-outlined">person</i></a>
         </div>
+
+        <!-- menu for mobile -->
+        <div class="mobile-nav">
+            <div class="nav-hamburger" onclick="toggleMobileMenu()">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+
+        <!-- mobile Menu -->
+        <div class="mobile-menu">
+            <div class="mobile-content">
+                <div class="mobile-btn">
+                    <a href="<?php echo home_url('cart'); ?>"><i class="material-symbols-outlined">shopping_bag</i></a>
+                </div>
+
+                <div class="mobile-menu-header">
+                    <span class="close-menu" onclick="toggleMobileMenu()"><i class="material-symbols-outlined">close</i></span>
+                </div>
+            </div>
+
+            <ul>
+                <li><a href="<?php echo home_url(); ?>">Home</a></li>
+                <li><a href="<?php echo home_url('#category'); ?>">Category</a></li>
+                <li><a href="<?php echo home_url('/products'); ?>">Products</a></li>
+                <li><a href="<?php echo home_url('/about-us'); ?>">About us</a></li>
+                <li><a href="<?php echo home_url('/contact'); ?>">Contacts</a></li>
+            </ul>
+
+            <div class="mobile-btn extra">
+                <a onclick="toggleTheme(); return false;" href="#"><i class="material-symbols-outlined">dark_mode</i></a>
+                <a href="#"><i class="material-symbols-outlined">person</i></a>
+            </div>
+        </div>
     </nav>
 
     <!-- dark mode -->
@@ -45,6 +80,11 @@
         // Save the theme
         if (localStorage.getItem("mode") === "dark-mode") {
         document.body.classList.add("dark-mode");
+        }
+
+        // mobile menu toggle
+        function toggleMobileMenu() {
+            document.querySelector('.mobile-menu').classList.toggle('open');
         }
     </script>
 </header>
