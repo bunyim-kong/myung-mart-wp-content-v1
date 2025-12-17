f5af6eb8768a0c2b7faff1f8490c8b84a53186b6
bunyim-kong <bunyimkong@gmail.com>
2025-12-17 15:51:28 +0700

    Contact page 70%

--- themes/my-ecommerce-theme/page-about-us.php
+++ themes/my-ecommerce-theme/page-about-us.php
@@ -14,6 +14,7 @@
 
 <section class="about-us-section2 container">
     <div class="about-us-content">
+        <div class="about-us-text">
             <h2>
                 About Us 
             </h2>
@@ -24,10 +25,13 @@
                 Our journey began with a deep love for Korean beauty and a mission to make authentic, high-quality products easier for everyone to access. We saw how hard it was for customers to find real, trustworthy skincare, so we created a place where quality, transparency, and reliability come first. By partnering directly with Korean brands, we aim to bridge the gap between Korea’s most-loved products and customers who deserve skincare they can truly depend on.
             </p>
         </div>
+
         <div class="about-us-pic">
             <img src="<?php echo get_template_directory_uri() . '/asset/images/about -image.jpg'; ?>" alt="" />
         </div>
+    </div>
 </section>
+
  <section class="about-us-section2 container">
         <div class="about-us-content2">
             <h2>
@@ -53,16 +57,17 @@
         </div>
         
     </section>
+    
     <section class="subscribe-section">
-    <div class="subscribe-content">
-        <h1>Subscribe to our Newsletter</h1>
-        <p>Sign up for our newsletter and be the first to know about the latest Korean <br>
-         skincare trends, exclusive offers, and expert beauty tips.</p>
+        <div class="subscribe-content">
+            <h1>Subscribe to our Newsletter</h1>
+            <p>Sign up for our newsletter and be the first to know about the latest Korean <br>
+            skincare trends, exclusive offers, and expert beauty tips.</p>
 
-        <div class="subscribe-form">
-            <input type="email" placeholder="Enter your email address" />
-            <button type="submit">Subscribe</button>
+            <div class="subscribe-form">
+                <input type="email" placeholder="Enter your email address" />
+                <button type="submit">Subscribe</button>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 <?php get_footer(); ?>
